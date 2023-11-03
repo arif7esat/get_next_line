dc9cc2f95654f30d843c3c7361fe0144f1d8385b
Arif Esat Güngör <220401038@ogr.comu.edu.tr>
2023-11-03 14:33:00 +0300

    [*** EN COMMIT MESSAGE 2 ***]
    This commit message introduces the 'get_next_line.h' header file, which provides a foundational interface for the 'get_next_line' function.
    The header file includes the following changes:
    
        1.  Include guards have been added to the header file.
            These guards include preprocessor directives used to ensure that the header file is included only once during compilation.
            The relevant directives are explained below:
    
                1.1 #ifndef GET_NEXT_LINE_H: This line ensures that the header is included only once (if not defined).
                1.2 #define GET_NEXT_LINE_H: This line marks the inclusion of the header file.
    
        2.  The prototype of the 'get_next_line' function has been defined.
        This function is responsible for reading data from a file, up to a specified 'BUFFER_SIZE,' and returning it line by line.
        Therefore, it takes a file descriptor as an argument.
    
        3.  During compilation, 'BUFFER_SIZE' can be specified to customize the read buffer's size.
    
        4.  If 'BUFFER_SIZE' is not defined during compilation, a default size of 3 is used.
    
    This header file serves as the foundation for the 'get_next_line' function, allowing for the configuration of the buffer size according to project requirements.
    
    [*** TR COMMIT MESAJ ***]
    
    Bu commit mesajı, 'get_next_line' fonksiyonu için temel bir arayüz sağlayan 'get_next_line.h' başlık dosyasını tanıtmaktadır.
    Başlık dosyası aşağıdaki değişiklikleri içermektedir:
    
        1.  Başlık dosyasında include koruması eklenmiştir.
            Bu koruma, başlık dosyasını derleme sırasında yalnızca bir kez dahil etmek için kullanılan önişlemci direktifler içermektedir.
            İlgili direktifler aşağıda açıklanmıştır:
    
                1.1 `#ifndef GET_NEXT_LINE_H`: Bu satır, (if not defined "tanımlanmamışsa") bir kez dahil edilmesini sağlamaktadır.
                1.2 `#define GET_NEXT_LINE_H`: Bu satır ise başlık dosyasının dahil etmektedir.
    
    2. 'get_next_line' fonksiyonunun prototipi tanımlanmıştır.
        Bu fonksiyon, bir dosyadan BUFFER_SIZE kadar okuma yaparak satır satır veri döndürme işlevini yerine getirmektedir.
        Bu nedenle, bir dosya tanımlayıcısı (file descriptor) almaktadır.
    
    3. Derleme sırasında 'BUFFER_SIZE' belirtilerek okuma tamponunun boyutunun özelleştirilmesine olanak tanınmaktadır.
    
    4. 'BUFFER_SIZE' tanımlanmamışsa, varsayılan boyut olarak 3 kullanılılmaktadır.
    
    Bu başlık dosyası, 'get_next_line' fonksiyonu için temel bir yapı sunmakta ve proje gereksinimlerine göre okuma tamponunun boyutunu yapılandırmayı mümkün kılmaktadır.

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,16 @@
 /*   By: agungor < agungor@student.42kocaeli.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 14:19:17 by agungor           #+#    #+#             */
-/*   Updated: 2023/11/03 14:23:57 by agungor          ###   ########.fr       */
+/*   Updated: 2023/11/03 14:32:35 by agungor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 3
+# endif
+
+char	*get_next_line(int fd);
+
+#endif
