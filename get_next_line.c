9780f896f19c6d704454cc46fe3974b685d07f12
Arif Esat Güngör <220401038@ogr.comu.edu.tr>
2023-11-03 19:42:36 +0300

    *** [TR COMMIT MESAJI] ***
    ['get_next_line.c' Dosyasında Yapılan Güncellemeler]
    
        1.  [get_next_line Fonksiyonda Yapılan Güncellemeler]
    
            Bu commit, 'get_next_line' fonksiyonunda yapılan değişiklikleri detaylı bir şekilde açıklamaktadır:
    
                1.  Fonksiyon girdi kontrolü: Fonksiyon, ilk önce 'fd'(file descriptor) dosya tanımlayıcısının ve 'BUFFER_SIZE' değerinin geçerliliğini doğrulamaktadır.
                    Eğer 'fd' sıfırdan küçük veya 'BUFFER_SIZE' sıfırdan küçük veya eşitse, fonksiyon NULL değeri döndürmektedir.
                    Bu kontroller, geçersiz girişleri ele alır ve hataların önlenmesine yardımcı olmaktadır.
    
                2.  'left_str' değişkenin işlevi: Bu değişken, 'get_next_line' fonksiyonunun önceki çağrılarından kalan veriyi saklamaktadır.
                    'static' olarak tanımlanmıştır çünkü her çağrı arasında veriyi saklamak ve güncellemek için kullanılacaktır.
                    Bu, verinin korunmasını ve işlenmesi görevini üstlenmektedir.
    
                3.  'ft_read_left_str' fonksiyonun : Bu fonksiyon, 'left_str' değişkenini güncellemek için kullanılmaktadır.
                    Mevcut 'left_str' içeriğini okuma işlemi ile güncellemekte ve yeniden kullanılmak üzere saklamaktadır.
                    Eğer 'left_str' NULL ise, fonksiyon yine NULL döndürmektedir.
                    Bu fonksiyon, dosyadan okunan veriyi geçici olarak 'left_str' içine aktarıp daha sonra bu veriyi işlemek üzere saklamaktadır.
    
                4.  'ft_get_line' fonksiyonu: Bu fonksiyon, 'left_str' içerisinden bir satırı almakta ve 'line' değişkenine kopyalamaktadır.
                    Bu işlem, 'get_next_line' fonksiyonunun temel amacını yerine getirmektedir.
                    Yani dosyadan okunmuş ama tam olarak hazırlanmamış veriyi almakta ve satır satır döndürmektedir.
    
                5.  'ft_get_new_str' fonksiyonu: Bu fonksiyon, kullanılan satırın 'left_str' içinden kaldırılmasını sağlayarak kalan veriyi günceller.
                    Bu, 'left_str' içeriğini temizlemek ve işlenen satırları çıkartmak için kullanılmaktadır.
    
    Bu güncellemeler, 'get_next_line' fonksiyonunun daha güvenli ve daha anlaşılır hale getirilmesi için yapılmıştır.
    Her adımın mantığı ve işlevi ayrıntılı bir şekilde açıklanmış ve bu sayede fonksiyonun nasıl çalıştığı daha iyi anlaşılabilir hale getirilmiştir.
    Ayrıca, 'left_str' değişkeninin neden 'static' olarak tanımlandığına da değinilmiştir.

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,22 @@
 /*   By: agungor < agungor@student.42kocaeli.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 11:17:01 by agungor           #+#    #+#             */
-/*   Updated: 2023/11/03 14:24:00 by agungor          ###   ########.fr       */
+/*   Updated: 2023/11/03 19:42:25 by agungor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	char static	*left_str;
+	char		*line;
+
+	if (fd <= 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	left_str = ft_read_left_str(left_str, fd);
+	if (!left_str)
+		return (NULL);
+	line = ft_get_line(left_str);
+	left_str = ft_get_new_str(left_str);
+}
