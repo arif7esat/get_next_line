d114d3a4b07973a007824b6a70380e5ea469bdcc
Arif Esat Güngör <220401038@ogr.comu.edu.tr>
2023-11-05 15:48:12 +0300

    [*** EN COMMIT MESSAGE ***]
        [This commit includes the 'stdlib' header file and some helper functions. The purpose of adding these functions is to be used inside the ft_read_left_str function].
    
            The functionality of each of these functions is described below:
    
            1. ft_strlen Function:
    
                Added because of its use in the 'strjoin' function.
                This function calculates the length of a given string of characters.
                It works by returning the number of characters in a string.
                It is like the standard strlen function used on every character array.
    
            2. ft_memcpy Function:
    
                Added because of its use in the 'strjoin' function.
                'ft_memcpy' performs a memory copy operation.
                It copies data from a source memory region to a destination memory region.
                It is like the standard memcpy function used for memory copying.
    
            3. ft_strjoin Function:
    
                This function joins two strings of characters and creates a new string.
                The first string (s1) and the second string (s2) are concatenated and a new string is returned.
    
            4. ft_strchr Function:
    
                'ft_strchr' looks for a specific character in a given character sequence.
                If the character is found, it returns the address of that character.
                If the character is not found, it returns NULL.
    
            5. ft_strdup Function:
    
                This function copies the given character array and returns a pointer to the copied array.
                It creates a new character array and copies the data into this new array.
    
    This commit introduces the operation and use of these five helper functions in the 'stdlib.h' header file.
    These functions can be used to perform basic functions that are often used in various projects and provide a basis for utilizing these functions in the project.
    
    [*** TR COMMIT MESAJ ***]
    
        [Bu commit, 'stdlib' başlık dosyasını ve bazı yardımcı fonksiyonları içermektedir. Bu fonksiyonların eklenmesindeki amaç ft_read_left_str fonksiyonun içide kullanılmasından kaynaklanmıştır.]
    
            Aşağıda bu fonksiyonların her birinin işlevselliği açıklanmıştır:
    
            1. ft_strlen Fonksiyonu:
    
                'strjoin' fonksiyonunda kullanılmasında dolayı eklenmiştir.
                Bu fonksiyon, verilen bir karakter dizisinin uzunluğunu hesaplar.
                Bir dizi içindeki karakter sayısını döndürerek çalışır.
                Her karakter dizisinde kullanılan standart strlen işlevi gibidir.
    
            2. ft_memcpy Fonksiyonu:
    
                'strjoin' fonksiyonunda kullanılmasında dolayı eklenmiştir.
                'ft_memcpy', bellek kopyalama işlemi yapar.
                Kaynak bir bellek bölgesinden hedef bir bellek bölgesine veri kopyalar.
                Bellek kopyalama işleminde kullanılan standart memcpy işlevi gibidir.
    
            3. ft_strjoin Fonksiyonu:
    
                Bu fonksiyon, iki karakter dizisini birleştirir ve yeni bir karakter dizisi oluşturur.
                İlk karakter dizisi (s1) ile ikinci karakter dizisi (s2) birleştirilir ve sonuçta yeni bir karakter dizisi döndürülür.
    
            4. ft_strchr Fonksiyonu:
    
                'ft_strchr', verilen bir karakter dizisinde belirli bir karakteri arar.
                İlgili karakter bulunursa, bu karakterin adresini döndürür.
                Eğer karakter bulunmazsa, NULL değeri döndürür.
    
            5. ft_strdup Fonksiyonu:
    
                Bu fonksiyon, verilen karakter dizisini kopyalar ve kopyalanan dizinin bir işaretçisini döndürür.
                Yeni bir karakter dizisi oluşturur ve veriyi bu yeni diziye kopyalar.
    
    Bu commit, 'stdlib.h' başlık dosyasının içindeki bu beş yardımcı fonksiyonun işleyişini ve kullanımını tanıtır.
    Bu fonksiyonlar, genellikle çeşitli projelerde kullanılan temel işlevleri gerçekleştirmek için kullanılabilir ve projede bu fonksiyonlardan faydalanmak için bir temel oluşturur.

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: agungor < agungor@student.42kocaeli.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 14:19:17 by agungor           #+#    #+#             */
-/*   Updated: 2023/11/05 13:25:18 by agungor          ###   ########.fr       */
+/*   Updated: 2023/11/05 15:43:52 by agungor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,11 @@
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 3
 # endif
+# include <sys/_types/_size_t.h>
 
 char	*get_next_line(int fd);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,12 +1,68 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*    get_next_line_utils.c                             :+:      :+:    :+:   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: agungor < agungor@student.42kocaeli.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/03 14:23:34 by agungor           #+#    #+#             */
-/*   Updated: 2023/11/03 14:24:03 by agungor          ###   ########.fr       */
+/*   Created: 2023/11/02 02:20:23 by agungor           #+#    #+#             */
+/*   Updated: 2023/11/05 15:45:24 by agungor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdlib.h>
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t	i;
+
+	if (!dst && !src)
+		return (0);
+	i = -1;
+	while (++i < n)
+		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+	return (dst);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*my_strjoin;
+	size_t	len1;
+	size_t	len2;
+
+	if (!s1 || !s2)
+		return (NULL);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	my_strjoin = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (!(my_strjoin))
+		return (NULL);
+	ft_memcpy(my_strjoin, s1, len1);
+	ft_memcpy(my_strjoin + len1, s2, len2);
+	my_strjoin[len1 + len2] = '\0';
+	return (my_strjoin);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = -1;
+	while (s[++i])
+		if (s[i] == (char)c)
+			return ((char *)s + i);
+	if (s[i] == (char)c)
+		return ((char *)s + i);
+	return (0);
+}
 
